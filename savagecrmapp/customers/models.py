495c6803d3521ac6911b6e7693e3ba1dfae51321
David Cabrera <yermandad@gmail.com>
2022-12-05 21:40:38 -0500

    add __str__ to models

--- savagecrmapp/customers/models.py
+++ savagecrmapp/customers/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,6 +10,13 @@ class   Customer(models.Model):
     c_phone = models.CharField(max_length=50)
     c_nick_name = models.CharField(max_length=200)
     c_password = models.CharField(max_length=200)
+    reg_date = models.DateTimeField("Date Registered")
+    
+    def __str__ (self):
+        return self.c_name
+
+    def reg_recently(self):
+        return self.reg_date >= timezone.now()- datetime.timedelta(months=1)
 
 class Service(models.Model):
     customer= models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -16,3 +25,10 @@ class Service(models.Model):
     product_bought= models.CharField(max_length=100)
     product_used= models.CharField(max_length=100)
     payment_way=models.CharField(max_length=100)
+    bougth_date= models.DateTimeField("Bougth Date")
+    
+    def __str__ (self):
+        return self.customer
+
+    def buy_recently(self):
+        return self.reg_date >= timezone.now()- datetime.timedelta(months=1)
